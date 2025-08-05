83511145e1ab4527d1793332e70a479b23df796e
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2025-08-04 20:15:06 -0400

    Created a resource to fetch users from the DB

--- src/server.ts
+++ src/server.ts
@@ -41,6 +41,33 @@ server.tool("create-user", "Create a new user in the DB", {
   }
 });
 
+server.resource(
+  "users", // name
+  "users://all", // Need to pass in a URI which is essentially a unique identifier that we can create however we want, but it must match a certain protocol. The protocol is relatively straightforward. Essentially, it's similar to a URL, but it can be referencing anything. It doesn't have to just be HTTP, for example, we could have a file:// in here, or we could even have our custom type, for example, we could say it's like a user's type(users://all) and we can have a link to all, for example. It doesn't really matter what you do. It just must match this similar format that you get with HTTP inside of a web browser URL. But it can match anything, whether it's a file, users, whatever. So, depending on your application, you may have your own standards you use, or you may create your own schemas. We're just gonna use this users protocol to start here because.That's just a custom one that we can create for our application.
+  {
+    description: "Get all users from DB",
+    title: "Users",
+    mimeType: "applicaiton/json"
+  }, 
+  // A function that takes the URI that is passed in
+  async uri => {
+    
+    const users = await import("./db/users.json", {
+      with: { type: 'json' }
+    }).then(ob => ob.default);
+    
+    return{
+      contents: [
+        { 
+          uri: uri.href, 
+          text: JSON.stringify(users),
+          mimeType: "applicaiton/json"
+        }
+      ]
+    }
+  }
+);
+
 async function  createUser(user:{
   name: string;
   email: string;
